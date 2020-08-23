abb65e83df90482bf0319e6403a016ac3bd77ba8
Walas de Oliveira Matos <walasoliveira1@hotmail.com>
2020-08-23 12:41:14 -0300

    Criação dos projetos.

---
+++ src/DevIO.Business/Models/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace MinhaAppMvcCompleta.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
